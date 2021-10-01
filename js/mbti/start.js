81e8e4149ea5fd808ba0ce2df88ce17df0fb5f90
jellyhani <62514874+jellyhani@users.noreply.github.com>
2021-10-01 15:09:29 +0900

    Add files via upload

---
+++ js/mbti/start.js
@@ -0,0 +1,51 @@
+const main = document.querySelector(".mbtiitem");
+const qna = document.querySelector(".qna");
+
+
+function addAnswer(answerText ,qIdex){
+    var a = document.querySelector('.answerBox');
+    var answer = document.createElement('button');
+    answer.classList.add('answerList');
+    a.appendChild(answer);
+    answer.style.display = "flex";
+    answer.innerHTML = answerText;
+    answer.addEventListener("click", function(){
+        var children = document.querySelectorAll('.answerList');
+        for(let i = 0; i < children.length; i++) {
+            
+            children[i].style.animation = "slideOut 0.5s";
+            setTimeout(() => {
+                setTimeout(() => {
+                    children[i].disabled = true;
+                    children[i].style.display = 'none';
+                }, 240)
+            }, 240)
+        }
+        goNext(++qIdex);
+    }, false);
+    
+}
+function goNext(qIdex){
+    var q = document.querySelector('.qBox');
+    q.innerHTML = qnaList[qIdex].q;
+    for(let i in qnaList[qIdex].a) {
+        addAnswer(qnaList[qIdex].a[i].answer, qIdex);
+    }
+}
+
+function begin(){
+    setTimeout(() => {
+        main.style.animation = "slideOut 0.5s";
+        setTimeout(() => {
+            main.style.display = "none";
+        }, 240)
+    }, 240);
+    setTimeout (() => {
+        qna.style.animation = "slideIn 0.5s";
+        setTimeout(() => {
+            qna.style.display = "block";
+        }, 240)
+        let qIdex = 0;
+        goNext(qIdex);
+    }, 240);
+}
---
+++ js/script.js
@@ -0,0 +1,37 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "160px";
+    document.getElementById("ani").style.marginLeft = "160px";
+    document.body.style.backgroundColor = "rgba(199,21,133,0.4)";
+
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("ani").style.marginLeft = "0";
+    document.body.style.backgroundColor = "pink";
+}
+
+// $('#MBTI').on('click', function () {
+//     $('#container').load('http://127.0.0.1:5500/mbti/index.html');
+// });
+// $('#Kakao').on('click', function () {
+//     $('#container').load('http://127.0.0.1:5500/mbti/kakao.html');
+// });
+// $('#HY').on('click', function () {
+//     $('#container').load('http://127.0.0.1:5500/mbti/hy.html');
+// });
+// $('#Album').on('click', function () {
+//     $('#container').load('http://127.0.0.1:5500/mbti/album.html');
+// });
+$('#MBTI').on('click', function() {
+  $('#span').load('https://hymbti.netlify.app/index.html');
+});
+$('#Kakao').on('click', function() {
+  $('#span').load('https://hymbti.netlify.app/kakao.html');
+});
+$('#HY').on('click', function() {
+  $('#span').load('https://hymbti.netlify.app/hy.html');
+});
+$('#Album').on('click', function() {
+  $('#span').load('https://hymbti.netlify.app/album.html');
+});
