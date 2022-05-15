0d5abdcc4097cd6f5490c8c5f64327fafdf003f0
jellyhani <62514874+jellyhani@users.noreply.github.com>
2022-05-15 18:48:54 +0900

    update album.js

--- js/album/album.js
+++ js/album/album.js
@@ -14,26 +14,7 @@ filterSelection("all")
     }
   }
 
-  function w3AddClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
-    }
-  }
-
-  function w3RemoveClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      while (arr1.indexOf(arr2[i]) > -1) {
-        arr1.splice(arr1.indexOf(arr2[i]), 1);
-      }
-    }
-    element.className = arr1.join(" ");
-  }
+  
   // Add active class to the current button (highlight it)
   var btnContainer = document.getElementById("myBtnContainer");
   var btns = btnContainer.getElementsByClassName("button");
